0ffb1f2dfcc6228a8f0775c9de17c03acf81d631
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-10-20 17:19:55 +0200

    Added SRF 3 & SRF 1 and Changed the strings.xml and Changed the Version to 1.0.2 and Version Code to 3 and Changed the Stream URL for Radio Pilatus to the right URL

--- app/src/main/java/com/freetime/rpj/MainActivity.java
+++ app/src/main/java/com/freetime/rpj/MainActivity.java
@@ -13,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
     private ExoPlayer exoPlayer;
     private final String sunshineUrl = "https://chmedia.streamabc.net/79-rsunshine-mp3-192-4746851?sABC=68s562rr%231%231699795900047_8633080%23puzrqvn-enqvb-jro&aw_0_1st.playerid=chmedia-radio-web&amsparams=playerid:chmedia-radio-web;skey:1760912110"; // replace with real stream
     private final String argoviaUrl = "https://chmedia.streamabc.net/79-argovia-mp3-192-3024993?sABC=68s5qo3r%230%23q7s809s74070n543n7p5213q6qr39235%23qverpg&aw_0_1st.playerid=direct&amsparams=playerid:direct;skey:1760942910"; // replace with real stream
-    private final String pilatusUrl = "http://stream.streambase.ch/radiopilatus/mp3-192/"; // replace with real stream
+    private final String pilatusUrl = "https://chmedia.streamabc.net/79-pilatus-mp3-192-4664468?sABC=68s5s266%230%23q7s809s74070n543n7p5213q6qr39235%23puzrqvn&aw_0_1st.playerid=chmedia&amsparams=playerid:chmedia;skey:1760948838"; // replace with real stream
+    private final String srf3Url = "http://livestreaming-node-2.srg-ssr.ch/srgssr/srf3/mp3/128"; // replace with real stream
+    private final String srf1Url = "http://livestreaming-node-2.srg-ssr.ch/srgssr/srf1/mp3/128"; // replace with real stream
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         MaterialButton SunPlayBtn = findViewById(R.id.SunPlayBTN);
         MaterialButton ArgPlayBtn = findViewById(R.id.ArgPlayBTN);
         MaterialButton PilPlayBtn = findViewById(R.id.PilPlayBTN);
+        MaterialButton SRF3PlayBtn = findViewById(R.id.SRF3PlayBTN);
+        MaterialButton SRF1PlayBtn = findViewById(R.id.SRF1PlayBTN);
 
         // Initialize ExoPlayer
         exoPlayer = new ExoPlayer.Builder(this).build();
@@ -40,6 +44,16 @@ public class MainActivity extends AppCompatActivity {
         PilPlayBtn.setOnClickListener(v -> {
             playStream(pilatusUrl, PilPlayBtn);
             Toast.makeText(MainActivity.this, "Playing Radio Pilatus", Toast.LENGTH_SHORT).show();
+        });
+
+        SRF3PlayBtn.setOnClickListener(v -> {
+            playStream(srf3Url, SRF3PlayBtn);
+            Toast.makeText(MainActivity.this, "Playing SRF 3", Toast.LENGTH_SHORT).show();
+        });
+
+        SRF1PlayBtn.setOnClickListener(v -> {
+            playStream(srf1Url, SRF1PlayBtn);
+            Toast.makeText(MainActivity.this, "Playing SRF 1", Toast.LENGTH_SHORT).show();
         });
     }
 
