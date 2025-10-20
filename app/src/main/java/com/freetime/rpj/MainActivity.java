d0af06b9dd4979b1d46bb6973eba5df9e665f77f
FreetimeMaker <jamie.achatz@s-stb.ch>
2025-10-20 12:11:09 +0000

    Changed App Version to 1.0.1 and Version Code to 2 and Added Radio Pilatus

--- app/src/main/java/com/freetime/rpj/MainActivity.java
+++ app/src/main/java/com/freetime/rpj/MainActivity.java
@@ -13,26 +13,33 @@ public class MainActivity extends AppCompatActivity {
     private ExoPlayer exoPlayer;
     private final String sunshineUrl = "https://chmedia.streamabc.net/79-rsunshine-mp3-192-4746851?sABC=68s562rr%231%231699795900047_8633080%23puzrqvn-enqvb-jro&aw_0_1st.playerid=chmedia-radio-web&amsparams=playerid:chmedia-radio-web;skey:1760912110"; // replace with real stream
     private final String argoviaUrl = "https://chmedia.streamabc.net/79-argovia-mp3-192-3024993?sABC=68s5qo3r%230%23q7s809s74070n543n7p5213q6qr39235%23qverpg&aw_0_1st.playerid=direct&amsparams=playerid:direct;skey:1760942910"; // replace with real stream
+    private final String pilatusUrl = "http://stream.streambase.ch/radiopilatus/mp3-192/"; // replace with real stream
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MaterialButton sunPlayBtn = findViewById(R.id.SunPlayBTN);
-        MaterialButton argPlayBtn = findViewById(R.id.ArgPlayBTN);
+        MaterialButton SunPlayBtn = findViewById(R.id.SunPlayBTN);
+        MaterialButton ArgPlayBtn = findViewById(R.id.ArgPlayBTN);
+        MaterialButton PilPlayBtn = findViewById(R.id.PilPlayBTN);
 
         // Initialize ExoPlayer
         exoPlayer = new ExoPlayer.Builder(this).build();
 
-        sunPlayBtn.setOnClickListener(v -> {
-            playStream(sunshineUrl, sunPlayBtn);
+        SunPlayBtn.setOnClickListener(v -> {
+            playStream(sunshineUrl, SunPlayBtn);
             Toast.makeText(MainActivity.this, "Playing Sunshine Radio", Toast.LENGTH_SHORT).show();
         });
 
-        argPlayBtn.setOnClickListener(v -> {
-            playStream(argoviaUrl, argPlayBtn);
+        ArgPlayBtn.setOnClickListener(v -> {
+            playStream(argoviaUrl, ArgPlayBtn);
             Toast.makeText(MainActivity.this, "Playing Radio Argovia", Toast.LENGTH_SHORT).show();
+        });
+
+        PilPlayBtn.setOnClickListener(v -> {
+            playStream(pilatusUrl, PilPlayBtn);
+            Toast.makeText(MainActivity.this, "Playing Radio Pilatus", Toast.LENGTH_SHORT).show();
         });
     }
 
